3ab8c4e5a88efa4b9357ad5741df24e25789ec10
przemyslaw.kozlowski <przemyslaw.kozlowski@xebia.com>
2025-03-18 22:44:19 +0100

    Basic moving
    Added basic moving, only happy path and mocks of npc talk and item
    gathering

--- game/main.py
+++ game/main.py
@@ -1,32 +1,81 @@
-from logo import print_logo_in
-from player import Player
-from world import world
-from npc import talk
+import os
+import time
+from game.logo import clear_sreen, print_logo_in
+from game.player import Player
+from game.world import world
+from game.npc import talk
 from colorama import init, Fore
+from utils.map import print_map
 
-def print_map(current):
+def go_forward(current_location):
+    direction=input("Dokąd się udajemy? ")
 
+    if direction in current_location["options"]:
+        return current_location["options"][direction]
+    else:
+        print("Chyba się pogubiliśmy! W tym kierunku nie możemy się udać! Spójrzmy jeszcze raz na mapę")
+        return ""
+
+
+def talk_to_npc(current_location):
+    print("Pozmawiam z miejscowymi!")
+
+def pick_item(current_location):
+    print("Sprawdzam, co tu jest!")
+
+def clear_screen():
+    os.system("clear")
 
 def game_loop():
-    player= Player("Bohater")
-    print(Fore.GREEN+f"Witaj, {player.name}!" + Fore.RESET)
+    player= Player("JANEK I JULEK")
+    basic_actions= {
+        "1": go_forward,
+        "2": talk_to_npc,
+        "3": pick_item
+    }
+
+    print(Fore.GREEN + f"\n🌟 Witajcie, {player.name}! 🌟" + Fore.RESET)
+
+    time.sleep(2)
+
     while True:
         current = world[player.location]
-        print(Fore.CYAN + f"\nJesteś w: {player.location}" + Fore.RESET)
-        print(current["desc"])
-        action= input("Co robisz ?").lower()
-        if action in current["options"]:
-            player.location = current["options"][action]
-        else:
-            print(Fore.YELLOW + talk(action) + Fore.RESET)
+
+        #clear_sreen()
+
+    #    print(current)
+
+        print(Fore.CYAN + f"\n📍 Jesteście w: {player.location}" + Fore.RESET)
+        print("\n" + Fore.LIGHTYELLOW_EX + current["desc"] + Fore.RESET + "\n")
+        print_map(current)
+
+        print_basic_actions()
+        print()
+        action= input("Co chcecie zrobić? Wpiszcie numer akcji: ").lower()
+        
+        if action in basic_actions:
+            new_location=basic_actions[action](current)
+            current = world[new_location]
+            player.location=new_location
+
+        #else:
+         #   print(Fore.YELLOW + talk(action) + Fore.RESET)
+
+def print_basic_actions():
+    print("🎒  1. Ruszam na przygodę!")
+    print("🗣   2. Ktoś tu jest! Pogadam z nim!")
+    print("🔍  3. Ooo! Coś tu leży! Sprawdzę!")
 
 def main():
-    print("Select Language")
-    print("1. English")
-    print("2. Polski")
+    print("═════════════════════════════════")
+    print("🌍  WITAJ W ŚWIECIE PRZYGÓD!  🌍")
+    print("═════════════════════════════════")
+    print("\nWybierz język:")
+    print("1. English 🇬 🇧")
+    print("2. Polski 🇵 🇱")
 
     while True:
-        choice = input("Enter your choice: ")
+        choice = input("🎮 Wpisz swój wybór: ")
 
         if choice == '1':
             print_logo_in('en')
--- utils/map.py
+++ utils/map.py
@@ -1,12 +1,39 @@
 def find_longest_word(words):
     return max(words, key=len)
 
-def print_all_locations(locations):
-    for location in locations:
-        print(f'[ {location} ]')
+def print_top(word_len, north):
+    top="─".ljust(word_len,"─")
+    empty=" ".ljust(word_len)
+    north_justed=(north + " (N)" if north else "").center(word_len)
 
-def find_most_north(locations):
-    for location in locations:
-        pass
+    print(f"┌{top}┬{top}┬{top}┐") 
+    print(f"│{empty}│{north_justed}│{empty}│")
+    print(f"├{top}┼{top}┼{top}┤")
+
+def print_middle(word_len,west, east):
+    west_justed=(west + " (W)" if west else "").center(word_len)
+    east_justed=(east + " (E)" if east else "").center(word_len)
+    here="X".center(word_len)
+
+    print(f"│{west_justed}│{here}│{east_justed}│")
+
+def print_bottom(word_len, south):
+    top="─".ljust(word_len,"─")
+    south_justed=(south + " (S)" if south else "").center(word_len)
+    empty=" ".ljust(word_len)
+
+    print(f"├{top}┼{top}┼{top}┤")
+    print(f"│{empty}│{south_justed}│{empty}│")
+    print(f"└{top}┴{top}┴{top}┘")
+
+def print_map(current):
+    directions = {"n": "", "s":"", "e":"", "w":""}
+
+    for key in directions:
+        directions[key]=current["options"].get(key,"")
         
-def create_
+    print("MAPA:")
+
+    print_top(40,directions["n"])
+    print_middle(40,directions["w"], directions["e"])
+    print_bottom(40,directions["s"])
