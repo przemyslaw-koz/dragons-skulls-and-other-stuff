519b73f4107f21201ea12c0c66102c8283f1dd9d
przemyslaw-koz <przemyslaw.koz@gmail.com>
2025-03-19 02:37:37 +0100

    npc talk
    added first functions body to arrange conversation with npc

--- game/npc.py
+++ game/npc.py
@@ -1,7 +1,36 @@
-responses = {"cześć": "Witaj, młody wędrowcze!", "gdzie jestem?": "Jesteś w magicznej krainie" }
+responses = {
+    "cześć": "Witaj, młody wędrowcze!",
+    "gdzie jestem?": "Jesteś w magicznej krainie",
+}
+
 
 def talk(player_input):
     return responses.get(player_input.lower(), "Nie rozumiem")
 
+
 def talk_in(location):
-    print("Porozmawiam z miejscowymi")
+    print("\n✨ **Porozmawiajmy z mieszkańcami wioski!** ✨")
+    print(
+        "Wybierzcie kogoś, kto Was interesuje, kierując się ich opisami. Być może imiona zdradzą Wam w trakcie rozmowy."
+    )
+    print("\nOto mieszkańcy, którzy mogą opowiedzieć Wam o tym magicznym miejscu:")
+
+    npcs = location["npcs"]
+
+    for index, (npc_name, npc_desc) in enumerate(npcs.items(), 1):
+        print(f"\n🌟 **Postać {index}:**")
+        print(f"  - {npc_desc}")
+
+    choice = int(input("\nWybierz numer postaci, z którą chcesz porozmawiać"))
+
+    if 1 <= choice <= len(npcs):
+        pass
+        # selected_npc_name = list(npcs.keys())[choice - 1]
+        # selected_npc_desc = npcs[selected_npc_name]
+        # print(f"\n🌟 **Zaczynasz rozmowę z {selected_npc_name}:**")
+        # print(f"  - {selected_npc_desc}")
+        # print(
+        #   f"  {selected_npc_name}: 'Witaj, podróżniku! Cieszę się, że zechciałeś ze mną porozmawiać!'"
+        # )
+    else:
+        print("\n❌ Niepoprawny wybór. Spróbuj ponownie.")
