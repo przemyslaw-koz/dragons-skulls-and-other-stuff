8fbd42625467a667681441b70c8dea1535e3adfa
przemyslaw.kozlowski <przemyslaw.kozlowski@xebia.com>
2025-03-19 01:46:27 +0100

    fixed test and formatting
    fixed map unit test and fixed formatting of main function

--- game/main.py
+++ game/main.py
@@ -1,18 +1,18 @@
-import os
 import time
 
-from game.player_actions import get_player_actions, print_basic_actions
+from colorama import Fore
+
+from game.npc import talk
 from game.player import Player
+from game.player_actions import get_player_actions, print_basic_actions
 from game.welcome import print_welcome
 from game.world import world
-from game.npc import talk 
-from colorama import Fore
 from utils.terminal import clear_screen
 
 
 def game_loop():
-    player= Player("JANEK I JULEK")
-    basic_actions=get_player_actions()
+    player = Player("JANEK I JULEK")
+    basic_actions = get_player_actions()
 
     print(Fore.GREEN + f"\n🌟 Witajcie, {player.name}! 🌟" + Fore.RESET)
 
@@ -21,28 +21,37 @@ def game_loop():
     while True:
         current = world[player.location]
 
-        #clear_screen()
+        # clear_screen()
 
-        print(Fore.CYAN + f"\n📍 Jesteście w: {player.location}" + Fore.RESET)
+        print(
+            Fore.CYAN
+            + f"\n📍 Jesteście w miejscu znanym jako: {player.location}"
+            + Fore.RESET
+        )
         print("\n" + Fore.LIGHTYELLOW_EX + current["desc"] + Fore.RESET + "\n")
 
         print()
         print(Fore.MAGENTA + "✨ Co dalej, dzielni podróżnicy? ✨" + Fore.RESET)
-        print(Fore.LIGHTWHITE_EX + "Wybierzcie kolejną akcję, podając jej numer:" + Fore.RESET)
+        print(
+            Fore.LIGHTWHITE_EX
+            + "Wybierzcie kolejną akcję, podając jej numer:"
+            + Fore.RESET
+        )
         print()
         print_basic_actions()
         print()
 
         action = input(Fore.CYAN + "🔮 Wasz wybór: " + Fore.RESET).strip().lower()
-        
+
         clear_screen()
 
         if action in basic_actions:
-            new_location=basic_actions[action](current)
+            new_location = basic_actions[action](current)
             if new_location:
-                player.location=new_location
+                player.location = new_location
         else:
             print(Fore.YELLOW + talk(action) + Fore.RESET)
+
 
 if __name__ == "__main__":
     print_welcome()
--- tests/test_map.py
+++ tests/test_map.py
@@ -1,20 +1,29 @@
 import unittest
 from io import StringIO
 from unittest.mock import patch
-from utils.map import find_longest_word
-from utils.map import print_all_locations
+
+from utils.map import find_longest_word, print_top
+
 
 class TestMap(unittest.TestCase):
+    def setUp(self):
+        self.maxDiff = None
+
     def test_find_longest_word(self):
-        result= find_longest_word(["test", "test1", "", "test11"])
+        result = find_longest_word(["test", "test1", "", "test11"])
         self.assertEqual(result, "test11")
 
     @patch("sys.stdout", new_callable=StringIO)
-    def test_location_printing(self, mock_stdout):
-        print_all_locations(["location1", "location2", "location3", "location4"])
-        output= mock_stdout.getvalue().strip()
-        self.assertEqual(output, "[ location1 ][ location2 ][ location3 ][ location4 ]")
+    def test_print_top(self, mock_stdout):
+        print_top(40, "north")
+        output = mock_stdout.getvalue().strip()
+        self.assertEqual(
+            output,
+            """┌────────────────────────────────────────┬────────────────────────────────────────┬────────────────────────────────────────┐
+│                                        │               north (N)                │                                        │
+├────────────────────────────────────────┼────────────────────────────────────────┼────────────────────────────────────────┤""",
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
-
