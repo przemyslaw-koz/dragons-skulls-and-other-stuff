1087b4ac27c76683bddd947b6ae8c4743ae59793
przemyslaw-koz <przemyslaw.koz@gmail.com>
2025-03-19 18:10:48 +0100

    formatting
    formatting to test git actions and to meet flake8 requirements

--- game/player_actions.py
+++ game/player_actions.py
@@ -4,15 +4,10 @@ from game.npc import talk_in
 
 
 def get_player_actions():
-    return {
-        "1": go_forward,
-        "2": talk_in,
-        "3": pick_item_in
-    }
+    return {"1": go_forward, "2": talk_in, "3": pick_item_in}
+
 
 def print_basic_actions():
     print("🎒  1. Ruszam na przygodę!")
     print("🗣   2. Ktoś tu jest! Pogadam z nim!")
     print("🔍  3. Ooo! Coś tu leży! Sprawdzę!")
-
-
