dc249d6a54fd20f5a7f84e9fb0f94f0cb162de20
przemyslaw-koz <przemyslaw.koz@gmail.com>
2025-03-20 15:41:58 +0100

    adding unit tests
    testing map

--- tests/test_map.py
+++ tests/test_map.py
@@ -1,29 +1,85 @@
-import unittest
-from io import StringIO
-from unittest.mock import patch
+import pytest
 
-from utils.map import find_longest_word, print_top
+from utils.map import find_longest_word, print_bottom, print_middle, print_top
 
 
-class TestMap(unittest.TestCase):
-    def setUp(self):
-        self.maxDiff = None
-
-    def test_find_longest_word(self):
-        result = find_longest_word(["test", "test1", "", "test11"])
-        self.assertEqual(result, "test11")
-
-    @patch("sys.stdout", new_callable=StringIO)
-    def test_print_top(self, mock_stdout):
-        print_top(40, "north")
-        output = mock_stdout.getvalue().strip()
-        self.assertEqual(
-            output,
-            """┌────────────────────────────────────────┬────────────────────────────────────────┬────────────────────────────────────────┐
-│                                        │               north (N)                │                                        │
-├────────────────────────────────────────┼────────────────────────────────────────┼────────────────────────────────────────┤""",
-        )
+def test_find_longest_word():
+    assert find_longest_word(["test1", "test22", "test333"])
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_print_top(capsys):
+    print_top(8, "pn")
+    captured = capsys.readouterr()
+    assert (
+        captured.out
+        == "┌────────┬────────┬────────┐\n│        │ pn (N) │        │\n├────────┼────────┼────────┤\n"
+    )
+
+
+def test_print_top_empty(capsys):
+    print_top(8, "")
+    captured = capsys.readouterr()
+    assert (
+        captured.out
+        == "┌────────┬────────┬────────┐\n│        │        │        │\n├────────┼────────┼────────┤\n"
+    )
+
+
+def test_print_middle_w_and_e(capsys):
+    print_middle(8, "za", "ws")
+    captured = capsys.readouterr()
+
+    assert captured.out == "│ za (W) │   X    │ ws (E) │\n"
+
+
+def test_print_middle_w(capsys):
+    print_middle(8, "za", "")
+    captured = capsys.readouterr()
+
+    assert captured.out == "│ za (W) │   X    │        │\n"
+
+
+def test_print_middle_e(capsys):
+    print_middle(8, "", "ws")
+    captured = capsys.readouterr()
+
+    assert captured.out == "│        │   X    │ ws (E) │\n"
+
+
+def test_print_middle_empty(capsys):
+    print_middle(8, "", "")
+    captured = capsys.readouterr()
+
+    assert captured.out == "│        │   X    │        │\n"
+
+
+def test_print_bottom(capsys):
+    print_bottom(8, "pd")
+    captured = capsys.readouterr()
+
+    assert (
+        captured.out
+        == "├────────┼────────┼────────┤\n│        │ pd (S) │        │\n└────────┴────────┴────────┘\n"
+    )
+
+
+def test_print_bottom_empty(capsys):
+    print_bottom(8, "")
+    captured = capsys.readouterr()
+
+    assert (
+        captured.out
+        == "├────────┼────────┼────────┤\n│        │        │        │\n└────────┴────────┴────────┘\n"
+    )
+
+
+def test_print_map():
+    pass
+    # directions = {"n": "", "s": "", "e": "", "w": ""}
+
+    # for key in directions:
+    #    directions[key] = current["options"].get(key, "")
+
+    # print_top(40, directions["n"])
+    # print_middle(40, directions["w"], directions["e"])
+    # print_bottom(40, directions["s"])
