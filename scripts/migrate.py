01a0e59ba22c86fb36da4f8b6942adb3614d166a
przemyslaw-koz <przemyslaw.koz@gmail.com>
2025-03-28 05:20:59 +0100

    fixing ruff suggestions

--- scripts/migrate.py
+++ scripts/migrate.py
@@ -1,25 +1,20 @@
-import json
 import os
 import sys
 
-from tinydb import Query, TinyDB
-
-# Uzyskanie bezwzględnej ścieżki do katalogu 'game'
-game_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../game"))
-sys.path.append(game_dir)
+from tinydb import TinyDB
 
 from game.world import world  # Importowanie mapy świata
 
-# Ścieżka do pliku JSON
+game_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../game"))
+sys.path.append(game_dir)
+
+
 db_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../db/world.json"))
 
-# Inicjalizacja bazy danych TinyDB
 db = TinyDB(db_path)
 
-# Usunięcie istniejących danych w bazie (jeśli jakieś istnieją)
 db.truncate()
 
-# Migracja danych do TinyDB
 for location_name, location_data in world.items():
     location_entry = {
         "name": location_name,
